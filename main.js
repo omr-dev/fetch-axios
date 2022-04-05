c92e625b9353dfbb6fb1f95c8538ffa70f014190
omer <peynirciomerfaruk@gmail.com>
2022-04-05 17:38:19 +0200

    fetch version completed

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-import './style.scss'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import "./style.scss";
+const url =
+  "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json";
+(async () => {
+  const response = await fetch(url);
+  const employees = await response.json();
+  console.log(employees);
+  document.querySelector("#app").innerHTML = `
+  <h1>Employees</h1>
+  <ul>
+  ${employees.map((employee) => {
+    return "<li>" + employee.lastName + " " + employee.firstName + "</li>";
+  }).join("")}
+  </ul>
+`;
+})();
